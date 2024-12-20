8f425bdf05fd467dd8f89657a0ebb2275c52d465
Ben <benogega@gmail.com>
2024-12-20 14:01:09 +0300

    A C++ program that receives input from the user and prints the same on the Console

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Benard Ogega
+Date: 19/12/2024
+This code is part of a series of my Programming learning process using C++
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char** argv) {
+
+    cout << "Enter your name: " <<endl;
+    string name;
+    cin >> name;
+    cout << "Hello World! " << name << "\n";
+    puts("Take this"); // can also use this. It is a Valid C++ 
+    return 0;
+}
